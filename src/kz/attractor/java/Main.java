7d1822b7f4f6f68a882ca0a6eeea4d0fb7972d42
Salmoor Nurzhanov <salmor662@gmail.com>
2023-02-26 15:18:16 +0600

    start project

--- src/kz/attractor/java/Main.java
+++ src/kz/attractor/java/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-            new Lesson45Server("localhost", 9879).start();
+            new Lesson45Server("localhost", 9889).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
